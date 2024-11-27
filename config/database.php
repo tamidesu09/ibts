8c6d0892f0e35c34266772a1eb17433ecd3039a4
Jann Marcus Flores <jannmarcusf@gmail.com>
2024-11-27 13:26:50 +0800

    added database config settings for hosting

--- config/database.php
+++ config/database.php
@@ -1,8 +1,11 @@
 <?php
+$DATABASE_URL = parse_url('DATABASE_URL');
+
 
 use Illuminate\Support\Str;
 
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +18,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +69,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => isset($DATABASE_URL["host"]) ? $DATABASE_URL["host"] : null,
+            'port' => isset($DATABASE_URL["port"]) ? $DATABASE_URL["port"] : null,
+            'database' => isset($DATABASE_URL["path"]) ? ltrim($DATABASE_URL["path"], "/") : null,
+            'username' => isset($DATABASE_URL["user"]) ? $DATABASE_URL["user"] : null,
+            'password' => isset($DATABASE_URL["pass"]) ? $DATABASE_URL["pass"] : null,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
@@ -125,7 +128,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
